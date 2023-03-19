e12cd0764d22f6cb2e4a3ed1c180da2fd61ea42d
nivorako <nivo.rakoto@yahoo.fr>
2023-03-19 11:02:09 +0100

    customize tooltip in bar

--- sportsee-frontend/src/components/chartBar/index.js
+++ sportsee-frontend/src/components/chartBar/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BarChart, Bar, XAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
+import { BarChart, Bar, XAxis, CartesianGrid, Tooltip, BoxPlot, ResponsiveContainer } from 'recharts'
 import { useEffect, useState } from 'react'
 
 import "./chartBar.css"
@@ -9,27 +9,38 @@ export default function ChartBar({userActivity}) {
   useEffect(() => {
     setData(userActivity)
   }, [userActivity])
-
-    return (  
-      <ResponsiveContainer width="100%" height="90%">
-        <BarChart
-          width={400}
-          height={300}
-          data={data}
-          margin={{
-            top: 70,
-            right: 30,
-            left: 20,
-            bottom: 0,
-          }}          
-        >
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="custom-tooltip">
+          <p>{`${payload[0].value} g`}</p>
+          <p>{`${payload[1].value}cal`}</p>
+        </div>
+      );
+    }
+    return null;
+  };
+  
+  return (  
+    <ResponsiveContainer width="100%" height="90%">
+      <BarChart
+        width={400}
+        height={300}
+        data={data}
+        margin={{
+          top: 70,
+          right: 30,
+          left: 20,
+          bottom: 0,
+        }}          
+      >
           <CartesianGrid strokeDasharray="1 1" vertical="" />
           <XAxis dataKey="name" />  
-          <Tooltip />       
-          <Bar yAxisId="kg" dataKey="poids" fill="red" barSize={7} radius={[10,10,0,0]}/>
+          <Tooltip content={<CustomTooltip />}/>       
+          <Bar yAxisId="kg" dataKey="poids" fill="red" barSize={7} radius={[10,10,0,0]}/>  
           <Bar yAxisId="cal" dataKey="calories" fill="black" barSize={7}  radius={[10,10,0,0]}/>
-        </BarChart>
-      </ResponsiveContainer>
-      
-    );
-  }
+      </BarChart>
+    </ResponsiveContainer>
+    
+  );
+}
